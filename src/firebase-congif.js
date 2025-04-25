796e561ab51259c0f9b9f345d12c6940e91c4705
yoenheep <yoen3212@naver.com>
2025-04-25 13:01:43 +0900

    배포중...ing

--- src/firebase-congif.js
+++ src/firebase-congif.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyU39C5CgxmjfI_yZgHYNukQPbzu25NH8",
-  authDomain: "pokemonapi-9e401.firebaseapp.com",
-  projectId: "pokemonapi-9e401",
-  storageBucket: "pokemonapi-9e401.firebasestorage.app",
-  messagingSenderId: "218413819271",
-  appId: "1:218413819271:web:ec33006b5228167f4b1f22",
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
